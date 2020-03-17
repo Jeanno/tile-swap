e8a1017385f8ebd0c024be4a35e3951d66b78d2c
Jeanno <jeanno.cheung@gmail.com>
2020-03-16 23:06:38 -0400

    Show last tile when complete

--- main.js
+++ main.js
@@ -2,14 +2,50 @@ let BOARD_SIZE = 3;
 let TILE_SIZE = 150;
 let LAST_TILE = BOARD_SIZE * BOARD_SIZE - 1;
 
+
 let tiles = [];
 let positions = [];
 let started = false;
+
+
+function isCorrect() {
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        for (let j = 0; j < BOARD_SIZE; j++) {
+            let id = i * BOARD_SIZE + j;
+            let pos = positions[id];
+            if (pos[0] != j || pos[1] != i) {
+                return false;
+            }
+
+            if (i == BOARD_SIZE - 1 && j == BOARD_SIZE - 1) {
+                return true;
+            }
+        }
+    }
+    return true;
+}
+
+
+function makeComplete() {
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        for (let j = 0; j < BOARD_SIZE; j++) {
+
+            let id = i * BOARD_SIZE + j;
+            positions[id] = [j, i];
+            if (i == BOARD_SIZE - 1 && j == BOARD_SIZE - 1) {
+                break;
+            }
+            updatePos(tiles[id], j, i);
+        }
+    }
+}
+
 
 function updatePos(tile, x, y) {
     tile.style.top = (y * 150) + 'px';
     tile.style.left = (x * 150) + 'px';
 }
+
 
 function genOnClick(id) {
     let c = function(e) {
@@ -23,9 +59,19 @@ function genOnClick(id) {
             positions[id] = empty;
             positions[LAST_TILE] = pos;
         }
+        if (isCorrect()) {
+            started = false;
+            updatePos(tiles[LAST_TILE], BOARD_SIZE - 1, BOARD_SIZE - 1);
+            setTimeout(complete, 700);
+        }
 
     };
     return c;
+}
+
+
+function complete() {
+    tiles[LAST_TILE].className = "tile";
 }
 
 
@@ -38,10 +84,11 @@ function randomizeTiles() {
         updatePos(tiles[i], temp[0], temp[1]);
     }
     // Hide last tile
-    tiles[LAST_TILE].style.display = "None";
+    tiles[LAST_TILE].className = "tile hide";
 
     started = true;
 }
+
 
 function init() {
     let tilesDom = document.getElementById("tiles");
@@ -63,6 +110,4 @@ function init() {
 }
 
 
-
-init();
-
+window.onload = init;
